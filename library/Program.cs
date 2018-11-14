2971c52cd1b44f23b02b289b6d0ac1fbc6995234
isaito-kurumizawa <ikuosaito1989@gmail.com>
2018-11-14 13:06:07 +0900

    Add files via upload

---
+++ library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace library
+{
+    public class libraryTest
+    {
+        public string ResultTest()
+        {
+            return "ResultTest OK!";
+        }
+    }
+}
